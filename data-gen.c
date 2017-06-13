0e9745ab998fbf19c5c262e0a1d98e2e14c71e2f
nhbrown <nicholas@hickson-brown.co.uk>
2017-06-13 16:13:04 +0200

    Updated basic data-gen.c body
    Tested basic implementation and made some changes, should be functioning as expected now.

--- data-gen.c
+++ data-gen.c
@@ -1,30 +1,33 @@
-#include stdio.h
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-const long seed;
-const int i;
-
-int main(int argc, constant char *argv[])
-{
-  switch(argc)
-  {
-    case 2 : 
-      seed = (unsigned long)time(NULL);
-      i = argv[1];
-      break;
-      
-    case 3 :
-      seed = argv[1];
-      i = argv[2];
-      break;
-      
-    default : 
-      printf("Invalid input for data-gen.c");
-  }
-  
-  test();
-}
+long seed;
+int i;
 
 void test()
 {
-  printf("The seed is %d and the number of particles is %d", seed, i);
+  printf("The seed is %d and the number of particles is %d.\n", seed, i);
+}
+
+int main(int argc, const char *argv[])
+{
+  switch(argc)
+  {
+    case 2 :
+      seed = (unsigned long)time(NULL);
+      i = atoi(argv[1]);
+      break;
+
+    case 3 :
+      seed = atol(argv[1]);
+      i = atoi(argv[2]);
+      break;
+
+    default :
+      printf("Invalid input for data-gen.c!\n");
+      exit(0);
+  }
+  
+  test();
 }
