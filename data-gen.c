c2f9804b3ca517c399f41e747eab0630b2c01e02
nhbrown <nicholas@hickson-brown.co.uk>
2017-06-13 18:41:13 +0200

    Added RNG
    Added Park and Miller generator to data-gen.c

--- data-gen.c
+++ data-gen.c
@@ -5,6 +5,23 @@
 long seed;
 int i;
 
+const m = 2147483647; /* modulos */
+const a = 48271;  /* multiplier */
+const q = 44488; /* (m / a) */
+const r = 3399; /* (m % a) */
+
+int rand_pos()
+{
+  if(seed <= 0)
+  {
+    seed += m;
+  }
+  
+  seed = a * (seed % q) - r * (seed / q);
+  
+  return seed;
+}
+
 void test()
 {
   printf("The seed is %d and the number of particles is %d.\n", seed, i);
