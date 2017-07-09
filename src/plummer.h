b0092a768234710dee788e698d58e9758a035bdc
nhbrown <nicholas@hickson-brown.co.uk>
2017-07-09 20:47:31 +0200

    Update plummer.h

--- src/plummer.h
+++ src/plummer.h
@@ -1,6 +1,6 @@
 #ifndef PLUMMER_H_
 #define PLUMMER_H_
 
-char * startPlummer(unsigned long seed, int N);
+char * startPlummer(unsigned long s, int amount, double timestep, double end_time);
 
 #endif // PLUMMER_H_
