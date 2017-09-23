d2c9e6886e70c60f74da8b09ff0d694ae39b7a8e
nhbrown <nicholas@hickson-brown.co.uk>
2017-09-23 17:55:48 +0200

    Update output.h

--- src/output.h
+++ src/output.h
@@ -3,7 +3,7 @@
 
 void createNames();
 
-void printIteration(double *mass, double complex **pos, double complex **vel, int iteration);
+void printIteration(double *mass, double complex **pos, double complex **vel, int iteration, int N);
 
 void printInitialConditions(unsigned long seed, int N, double M, double R, double G, 
                             double timestep, double end_time, double *mass, double complex **pos, double complex **vel);
