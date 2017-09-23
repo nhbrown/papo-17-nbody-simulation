42a58b7c8fbf11bbbbd3eb2f664c6fd48e4c4ecd
nhbrown <nicholas@hickson-brown.co.uk>
2017-09-23 17:46:21 +0200

    Update hermite.h

--- src/hermite.h
+++ src/hermite.h
@@ -1,6 +1,7 @@
 #ifndef HERMITE_H_
 #define HERMITE_H_
 
-void startHermite(int particles, double timestep, double end, char *folder);
+void startHermite(int particles, double timestep, double end, double *mass, double complex **pos, double complex **vel, 
+                  double complex **acc, double complex **jerk);
 
 #endif // HERMITE_H_
