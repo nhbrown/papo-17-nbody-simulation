7b1a3f7af8fa2aa2fd2222eeb3cf62a7a628c11c
nhbrown <nicholas@hickson-brown.co.uk>
2017-10-03 23:37:54 +0200

    Create ediag.c

---
+++ src/ediag.c
@@ -0,0 +1,15 @@
+double e_kinetic = 0;
+double e_potential = 0;
+double e_total = 0;
+
+void kinetic_energy(double complex *mass, double complex **vel)
+{
+}
+
+void potential_energy(double complex *mass, double complex **pos)
+{
+}
+
+void energy_diagnostics(double complex *mass, double complex **pos, double complex **vel)
+{
+}
