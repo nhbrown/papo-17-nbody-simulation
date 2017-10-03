50becfa5bca772343fed6c7802d78bc8bb6b6fc0
nhbrown <nicholas@hickson-brown.co.uk>
2017-10-03 21:32:37 +0200

    Updated startHermite()

--- src/hermite.h
+++ src/hermite.h
@@ -1,7 +1,7 @@
 #ifndef HERMITE_H_
 #define HERMITE_H_
 
-void startHermite(int particles, double timestep, double end_time, double *mass, double complex **pos, double complex **vel, 
+void startHermite(int N, int DIM, double dt, double end_time, double *mass, double complex **pos, double complex **vel, 
                   double complex **acc, double complex **jerk);
 
 #endif // HERMITE_H_
