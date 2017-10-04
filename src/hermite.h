ebe3f9f40a572bf2fb66385a2457648a0e64fa09
nhbrown <nicholas@hickson-brown.co.uk>
2017-10-04 23:59:15 +0200

    Added prototypes

--- src/hermite.h
+++ src/hermite.h
@@ -1,6 +1,12 @@
 #ifndef HERMITE_H_
 #define HERMITE_H_
 
+void acc_jerk(int N, int DIM, double *mass, double complex **pos, double complex **vel, 
+              double complex **acc, double complex **jerk);
+
+void hermite(int N, int DIM, double dt, double *mass, double complex **pos, 
+             double complex **vel, double complex **acc, double complex **jerk);
+
 void startHermite(int N, int DIM, double dt, double end_time, double *mass, double complex **pos, double complex **vel, 
                   double complex **acc, double complex **jerk);
 
