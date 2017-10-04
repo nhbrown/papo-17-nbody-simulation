e43524ec2b6dc201c07e1d6a915a329d8bfeeb1a
nhbrown <nicholas@hickson-brown.co.uk>
2017-10-04 02:34:42 +0200

    Update ediag.h

--- src/ediag.h
+++ src/ediag.h
@@ -1,6 +1,6 @@
 #ifndef EDIAG_H_
 #define EDIAG_H_
 
-void energy_diagnostics(int N, double *mass, double complex **pos, double complex **vel);
+void energy_diagnostics(int N, int DIM, double *mass, double complex **pos, double complex **vel);
 
 #endif // EDIAG_H_
