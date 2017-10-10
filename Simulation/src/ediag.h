4c5d2b284bf984dd2ebdbd23fd837c8299c9cb07
nhbrown <nicholas@hickson-brown.co.uk>
2017-10-10 17:15:14 +0200

    Removed DIM

--- Simulation/src/ediag.h
+++ Simulation/src/ediag.h
@@ -1,10 +1,10 @@
 #ifndef EDIAG_H_
 #define EDIAG_H_
 
-void kinetic_energy(int N, int DIM, double *mass, double complex **vel);
+void kinetic_energy(int N, double *mass, double complex **vel);
 
-void potential_energy(int N, int DIM, double *mass, double complex **pos);
+void potential_energy(int N, double *mass, double complex **pos);
 
-void energy_diagnostics(int N, int DIM, int marker, double *mass, double complex **pos, double complex **vel);
+void energy_diagnostics(int N, int marker, double *mass, double complex **pos, double complex **vel);
 
 #endif // EDIAG_H_
