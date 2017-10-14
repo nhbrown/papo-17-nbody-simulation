336040e1ea4d947513830039a36a5ecbf5db31c7
nhbrown <nicholas@hickson-brown.co.uk>
2017-10-14 15:50:13 +0200

    Adapted to new data structure

--- Simulation/src/hermite.c
+++ Simulation/src/hermite.c
@@ -21,15 +21,15 @@
 #include "ediag.h"
 
 /* calculates acceleration and jerk for all particles */
-void acc_jerk(int N, int DIM, double *mass, double complex **pos, double complex **vel, 
-              double complex **acc, double complex **jerk)
+void acc_jerk(int N, int DIM, double *mass, double complex *pos, double complex *vel, 
+              double complex *acc, double complex *jerk)
 { 
   /* default values for acceleration and jerk */
   for(int i = 0; i < N; ++i)
   {
-    for(int k = 0; k < 3; ++k)
+    for(int j = 0; j < 3; ++j)
     {
-      acc[i][k] = jerk[i][k] = 0;
+      acc[i + j] = jerk[i + j] = 0;
     }
   }
   
@@ -52,8 +52,8 @@ void acc_jerk(int N, int DIM, double *mass, double complex **pos, double complex
       /* filling the arrays described above */
       for(int k = 0; k < DIM; ++k)
       {
-        rji[k] = pos[j][k] - pos[i][k];
-        vji[k] = vel[j][k] - vel[i][k];
+        rji[k] = pos[j + k] - pos[i + k];
+        vji[k] = vel[j + k] - vel[i + k];
        
         r2 += rji[k] * rji[k];
         v2 += vji[k] * vji[k];
@@ -78,11 +78,11 @@ void acc_jerk(int N, int DIM, double *mass, double complex **pos, double complex
         da[k] = rji[k] / r3;
         dj[k] = (vji[k] - 3 * rv * rji[k]) / r3;
        
-        acc[i][k] += mass[j] * da[k];
-        acc[j][k] -= mass[i] * da[k];
+        acc[i + k] += mass[j] * da[k];
+        acc[j + k] -= mass[i] * da[k];
        
-        jerk[i][k] += mass[j] * dj[k];                
-        jerk[j][k] -= mass[i] * dj[k];  
+        jerk[i + k] += mass[j] * dj[k];                
+        jerk[j + k] -= mass[i] * dj[k];  
       }
     }
   }
@@ -93,23 +93,23 @@ Implementation of the Hermite scheme, calculates new positions and velocities fo
 For mathematical expressions used, see: http://www.ub.uni-heidelberg.de/archiv/6553 (Section 4.5) 
 Further information: Kokubo E., Yoshinaga K., Makino J., 1998, MNRAS 297, 1067
 */
-void hermite(int N, int DIM, double dt, double *mass, double complex **pos, 
-             double complex **vel, double complex **acc, double complex **jerk)
+void hermite(int N, int DIM, double dt, double *mass, double complex *pos, 
+             double complex *vel, double complex *acc, double complex *jerk)
 {
   /* storing positions, velocities, acceleration and jerk from last iteration */
-  double complex old_pos[N][DIM];
-  double complex old_vel[N][DIM];  
-  double complex old_acc[N][DIM];
-  double complex old_jerk[N][DIM];
+  double complex old_pos[N * DIM];
+  double complex old_vel[N * DIM];  
+  double complex old_acc[N * DIM];
+  double complex old_jerk[N * DIM];
   
   for(int i = 0; i < N; ++i)
   {
     for(int j = 0; j < DIM; ++j)
     {
-      old_pos[i][j] = pos[i][j];
-      old_vel[i][j] = vel[i][j];
-      old_acc[i][j] = acc[i][j];
-      old_jerk[i][j] = jerk[i][j];
+      old_pos[i + j] = pos[i + j];
+      old_vel[i + j] = vel[i + j];
+      old_acc[i + j] = acc[i + j];
+      old_jerk[i + j] = jerk[i + j];
     }
   }
   
@@ -118,8 +118,8 @@ void hermite(int N, int DIM, double dt, double *mass, double complex **pos,
   {
     for(int j = 0; j < DIM; ++j)
     {
-      pos[i][j] += vel[i][j] * dt + acc[i][j] * ((dt * dt)/2) + jerk[i][j] * ((dt * dt * dt)/6);
-      vel[i][j] += acc[i][j] * dt + jerk[i][j] * ((dt * dt)/2);
+      pos[i + j] += vel[i + j] * dt + acc[i + j] * ((dt * dt)/2) + jerk[i + j] * ((dt * dt * dt)/6);
+      vel[i + j] += acc[i + j] * dt + jerk[i + j] * ((dt * dt)/2);
     }
   }
   
@@ -131,15 +131,15 @@ void hermite(int N, int DIM, double dt, double *mass, double complex **pos,
   {
     for (int j = 0; j < DIM; ++j)
     {
-      vel[i][j] = old_vel[i][j] + (old_acc[i][j] + acc[i][j]) * (dt/2) + (old_jerk[i][j] - jerk[i][j]) * ((dt * dt)/12);       
-      pos[i][j] = old_pos[i][j] + (old_vel[i][j] + vel[i][j]) * (dt/2) + (old_acc[i][j] - acc[i][j]) * ((dt * dt)/12);
+      vel[i + j] = old_vel[i + j] + (old_acc[i + j] + acc[i + j]) * (dt/2) + (old_jerk[i + j] - jerk[i + j]) * ((dt * dt)/12);       
+      pos[i + j] = old_pos[i + j] + (old_vel[i + j] + vel[i + j]) * (dt/2) + (old_acc[i + j] - acc[i + j]) * ((dt * dt)/12);
     }
   }
 }
 
 /* entry point for the Hermite integrator, starts computation and continues until end of simulation is reached */
-void startHermite(int N, int DIM, double dt, double end_time, double *mass, double complex **pos, double complex **vel, 
-                  double complex **acc, double complex **jerk)
+void startHermite(int N, int DIM, double dt, double end_time, double *mass, double complex *pos, double complex *vel, 
+                  double complex *acc, double complex *jerk)
 {
   double time = 0.0; /* default time */
   int iterations = 0; /* iteration counter, iteration 0 is equal to initial conditions */
@@ -151,7 +151,7 @@ void startHermite(int N, int DIM, double dt, double end_time, double *mass, doub
   {
     ++iterations; /* increment iteration counter from last iteration to current iteration */
     hermite(N, DIM, dt, mass, pos, vel, acc, jerk); /* calculate movement for current iteration */
-    printIteration(mass, pos, vel, iterations, N); /* print current iteration */
+    printIteration(N, iterations, mass, pos, vel); /* print current iteration */
     energy_diagnostics(N, DIM, mass, pos, vel); /* get energy diagnostics for current iteration */
     time += dt; /* add timestep to current time to advance to next iteration */
   }
