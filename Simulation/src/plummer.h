01684ecb2109523c7beb992d5c866db2091d0f13
nhbrown <nicholas@hickson-brown.co.uk>
2017-10-21 19:25:04 +0200

    Update prototype

--- Simulation/src/plummer.h
+++ Simulation/src/plummer.h
@@ -5,7 +5,7 @@ double rrand(double low, double high);
 
 void plummer(int N, double *mass, double complex *pos, double complex *vel, int i, int mi, double M, double R);
 
-void center_of_mass_adjustment(int N, double *mass, double complex *pos, double complex *vel);
+void center_of_mass_adjustment(int N, int DIM, double *mass, double complex *pos, double complex *vel);
 
 void startPlummer(unsigned long s, int N, int DIM, double *mass, double complex *pos, double complex *vel, 
                   double M, double R);
